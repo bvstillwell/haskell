6a238ae4083fdeb8c705885663fb86c86e6d67bd
brad <brad.stillwell@gmail.com>
2015-02-19 01:16:34 +1300

    Update to code

--- euler/46.hs
+++ euler/46.hs
@@ -8,8 +8,8 @@ isComposite = (>1) . length . factors
 composites = filter isComposite [4..]
 oddComposites = filter odd composites
 
---goldBachs :: Integer -> [Integer]
+isGoldBachs :: Integer -> Bool
 isGoldBachs x = not $ null $ take 1 [ (value, prime, square) | prime <- takeWhile (<x) primes, square <- [1..wholeSqrt x], let value = prime + 2 * (square ^ 2), value == x]
 
-
-answer = take 1 $ dropWhile isGoldBachs $ oddComposites
+answer :: Integer
+answer = head $ dropWhile isGoldBachs oddComposites
