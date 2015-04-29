c96728fbdeea918260f70251fd756f4491aa0537
brad <brad.stillwell@gmail.com>
2015-04-30 00:02:59 +1200

    All compiling, need to test now

--- euler/bithard.hs
+++ euler/bithard.hs
@@ -1,82 +1,98 @@
 import qualified Data.Set as S
 
-
-
 data Bit = X | O | V Int deriving (Eq, Ord, Show)
 
-data AndSet = AB Bit | AS (S.Set Bit) deriving (Show)
-data XorSet = XB Bit | XS (S.Set AndSet) | XAS (S.Set AndSet) deriving (Show)
-
-newtype NewTypeInt = N Int
+data Expr = B Bit | AS (S.Set Expr) | XS (S.Set Expr)
 	deriving (Eq, Ord, Show)
 
---instance Eq Bit where
---	(V a) == (V b) = a == b
---	X == X = True
---	O == O = True
---	_ == _ = False
 
--- turn 2 bits into a set
--- posibilities
--- {} 
--- {X}
--- {Vn, Vn+1, ... X}
---and' :: Bit -> Bit -> S.Set Bit
---and' O _ = S.empty
---and' _ O = S.empty
---and' a b = S.fromList [a,b]
+-- *****************************
+-- NOT
+-- *****************************
+--Not a bit
+notBit :: Bit -> Expr
+notBit O = B X
+notBit X = B O
+notBit (V v) = xorBitBit X (V v)
 
--- turn 2 bits into a set
--- Possibilites
--- {}
--- {X}
--- {Vn, Vn+1, ... X}
---xor' :: Bit -> Bit -> S.Set Bit
---xor' a b
---	| a == b = S.empty
---	| a == O = S.singleton b
---	| b == O = S.singleton a
---	| otherwise = S.fromList [a, b]
+--Not an expr
+notExpr :: Expr -> Expr
+notExpr (B a) = notBit a
+notExpr s = xorBitExpr X s
 
 
---and' :: Bit -> S.Set Bit -> S.Set Bit
---and' O _ = S.empty
---and' a b = S.insert a b
-
-xor' :: Bit -> S.Set Bit -> S.Set Bit
-xor' O b = b
-xor' a b
-	| S.member a b = S.delete a b
-	| otherwise = S.insert a b
-
-
+-- *****************************
+-- AND
+-- *****************************
 --And 2 bits together
-andBitBit :: Bit -> Bit -> AndSet
-andBitBit O _ = AB O --Minimise Always minimise O
-andBitBit _ O = AB O
-andBitBit a X = AB a --don't care about X's
-andBitBit X b = AB b
+--Complete
+andBitBit :: Bit -> Bit -> Expr
+andBitBit O _ = B O --Minimise Always minimise O
+andBitBit _ O = B O
+andBitBit a X = B a --don't care about X's
+andBitBit X b = B b
 andBitBit a b
-	| a == b = AB a	
-	| otherwise = AS (S.fromList [a, b]) --Promote to a set
+	| a == b = B a	
+	| otherwise = AS (S.fromList [B a, B b]) --Promote to a set
 
 --Mix a bit with an and set
-andBitSet :: Bit -> AndSet -> AndSet
-andBitSet a (AB b) = andBitBit a b -- These are 2 bits
-andBitSet X (AS b) = AS b -- X's do nothing
-andBitSet a (AS b)
-	| S.null b = error "Shouldn't happen"
-	| S.size b == 1 = error "Shouldn't happen"
-	| otherwise = AS (S.insert a b)
+andBitExpr :: Bit -> Expr -> Expr
+andBitExpr O _ = B O
+andBitExpr X b = b -- X's do nothing
+andBitExpr (V v) (B b) = andBitBit (V v) b
+andBitExpr (V v) (AS as) = AS (S.insert (B (V v)) as) --Simple insert
+--Here we create a new XS from AND'ing (V n) with each item in the XS
+andBitExpr (V v) (XS xs) = foldr1 xorExprExpr $ map (andBitExpr (V v)) (S.toList xs)
 
---Mix 2 and sets together
-andSetSet :: AndSet -> AndSet -> AndSet
-andSetSet (AB a) (AB b) = andBitBit a b
-andSetSet (AB a) (AS b) = andBitSet a (AS b)
-andSetSet (AS a) (AB b) = andBitSet b (AS a)
-andSetSet (AS a) (AS b)
-	| S.null a = error "Shouldn't happen"
-	| S.size a == 1 = error "Shouldn't happen"
-	| S.null b = error "Shouldn't happen"
-	| S.size b == 1 = error "Shouldn't happen"
-	| otherwise = AS (S.union a b)
+andExprExpr :: Expr -> Expr -> Expr
+--Bit bit
+andExprExpr (B b1) (B b2) = andBitBit b1 b2
+--Simple append in most cases
+andExprExpr (B b) s = andBitExpr b s
+andExprExpr s (B b) = andBitExpr b s
+andExprExpr (AS as1) (AS as2) = AS (S.union as1 as2)
+--XOR the cartesion product of the sets (AND them)
+andExprExpr (XS xs1) (XS xs2) = foldr1 xorExprExpr [andExprExpr x1 x2 | x1<-S.toList xs1, x2<-S.toList xs2]
+--Map XS across AS and XOR everything.
+andExprExpr (AS as) (XS xs) = foldr1 xorExprExpr $ map (andExprExpr (AS as)) (S.toList xs)
+andExprExpr (XS xs) (AS as) = andExprExpr (AS as) (XS xs)
+
+
+
+-- *****************************
+-- XOR
+-- *****************************
+--Xor 2 bits together
+xorBitBit :: Bit -> Bit -> Expr
+xorBitBit O a = B a
+xorBitBit a O = B a
+xorBitBit a b	
+	| a == b = B O
+	| otherwise = XS (S.fromList [B a, B b]) --Promote to a set
+
+xorBitExpr :: Bit -> Expr -> Expr
+xorBitExpr O b = b --Ignore O
+xorBitExpr b1 (B b2) = xorBitBit b1 b2
+xorBitExpr b (XS xs) 
+	| S.member (B b) xs = XS (S.delete (B b) xs) --Xor remove if already in list
+	| otherwise = XS (S.insert (B b) xs)
+xorBitExpr b (AS as) = XS (S.fromList [(B b), (AS as)])
+
+xorExprExpr :: Expr -> Expr -> Expr
+xorExprExpr (B b1) (B b2) = xorBitBit b1 b2
+xorExprExpr (B b) s = xorBitExpr b s
+xorExprExpr s (B b) = xorBitExpr b s
+--XOR for here is the difference
+xorExprExpr (XS xs1) (XS xs2)
+	| S.null diff = B O
+	| otherwise = XS diff
+	where diff = S.difference xs1 xs2
+--Test for equality
+xorExprExpr (AS as1) (AS as2)
+	| as1 == as2 = B O
+	| otherwise = XS (S.fromList [(AS as1), (AS as2)])
+--Add the AS to the XS if it doesn't exist
+xorExprExpr (AS as) (XS xs)
+	| S.member (AS as) xs = XS (S.delete (AS as) xs) --Xor remove if already in list
+	| otherwise = XS (S.insert (AS as) xs)
+xorExprExpr (XS xs) (AS as) = xorExprExpr (AS as) (XS xs)
